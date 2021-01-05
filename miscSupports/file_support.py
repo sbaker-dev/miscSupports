fcd67ca9d9ed28804378848c490ce804eeba3c31
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-01-05 12:09:49 +0000

    common file supporting methods

---
+++ miscSupports/file_support.py
@@ -0,0 +1,64 @@
+import json
+import yaml
+import os
+
+
+def directory_iterator(directory, file_only=True):
+    """
+    This takes a directory and returns a list of entries within that directory, if file_only is selected only
+    files as apposed to directories will be returned
+
+    :param directory: The directory you wish to iterate through
+    :type directory: str
+
+    :param file_only: Defaults to true where the return is just a list of files
+    :type file_only: bool
+
+    :return: List of entries from the directory
+    :rtype: list
+    """
+
+    if file_only:
+        return [file for file in os.listdir(directory) if os.path.isfile(f"{directory}/{file}")]
+    else:
+        return [file for file in os.listdir(directory)]
+
+
+def read_json(path):
+    """
+    Read in a json file
+    """
+    with open(path) as ff:
+        return json.load(ff)
+
+
+def write_json(write_directory, write_data, write_name):
+    """
+    This writes out a json file
+
+    :param write_directory: Where to write the json file
+    :type write_directory: str
+
+
+    :param write_name: Name of the database you want to write
+    :type write_name: str
+
+    :param write_data: Data you want to write out to a json file
+    :type: write_data: Any
+
+    :return: Nothing, just write out the file to the specified directory named the specified name
+    :rtype: None
+    """
+    with open(f"{write_directory}/{write_name}.txt", "w", encoding="utf-8") as json_saver:
+        json.dump(write_data, json_saver, ensure_ascii=False, indent=4, sort_keys=True)
+
+
+def load_yaml(path_to_file):
+    """
+    Load the yaml file from package into scope
+    """
+    with open(path_to_file, "r") as f:
+        try:
+            return yaml.safe_load(f)
+        except yaml.YAMLError:
+            raise yaml.YAMLError
