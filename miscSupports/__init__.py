ad29ce44ba0f43149e912cfa741f0bb67a307d3e
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-05-10 20:49:59 +0100

    Allow for byte_support code to be accessed via __init__.py

--- miscSupports/__init__.py
+++ miscSupports/__init__.py
@@ -3,3 +3,4 @@ from miscSupports.misc import *
 from miscSupports.file_support import *
 from miscSupports.shapely_support import *
 from miscSupports.dates_and_time import *
+from miscSupports.byte_supports import *
